fb90ca7088a290953c2084b44f1c81459b83e8e3
glo1337 <vacys1337@gmail.com>
2025-04-30 13:30:34 +0500

    Каналы

--- 1-concurrency/main.go
+++ 1-concurrency/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
+func main() {
+	sliceCh := make(chan int)
+	powCh := make(chan float64)
+
+	go func() {
+		slice := createSlice(10)
+		for _, v := range slice {
+			sliceCh <- v
+		}
+		close(sliceCh)
+	}()
+
+	go func() {
+		for v := range sliceCh {
+			powCh <- math.Pow(float64(v), 2)
+		}
+		close(powCh)
+	}()
+
+	for v := range powCh {
+		fmt.Println(v)
+	}
+}
+
+func createSlice(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = rand.Intn(101)
+	}
+
+	return slice
 }
