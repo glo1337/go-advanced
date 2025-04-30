45813d6ab0707e3461d475b622d2c3af26916252
glo1337 <vacys1337@gmail.com>
2025-04-30 15:44:57 +0500

    random-api

---
+++ 2-random-api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+
+func main() {
+	router := http.NewServeMux()
+	NewRandomHandler(router)
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	server.ListenAndServe()
+}
